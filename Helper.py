8e19f91c8ea9ad0875a8eddc1fcbcfdf28d6bb61
Will <bicknell.william@gmail.com>
2020-02-03 13:39:25 -0500

    Initial commit Helps with searching log file, cleaning up files created by tracking app

---
+++ Helper.py
@@ -0,0 +1,202 @@
+"""
+#    FILE:          Helper.py
+#    PROGRAMMER:    William Bicknell
+#    FIRST VERSION: February 3, 2020
+#    DESCRIPTION:   This file runs a helper that can clean files and search the log file.
+#                   The main purpose for this is to help with development.
+"""
+
+import Logger
+import Metrics
+import os
+
+
+def DisplayMenu(message, options):
+    """
+    Displays a message and menu options.
+    
+    Args:
+    message - The message to display
+    options - A list of menu options to display
+    """
+    count = 1
+    
+    print("\n%s\n" % message)
+    for option in options:
+        print("%d) %s" % (count, option))
+        count += 1
+
+
+def DisplayInputError():
+    """
+    Displays a user input error message.
+    """
+    print("Invalid input!")
+    print("Please enter the corresponding number for one of the options above!\n")
+    
+
+def DeleteFile(fileName):
+    """
+    Deletes the specified file if it exists.
+    
+    Args:
+    fileName - The file to delete
+    """
+    if (os.path.exists(fileName)):
+        os.remove(fileName)
+        
+        
+def Confirm(message):
+    """
+    Gets a yes or no from the user corresponding to a message.
+
+    Args:
+    message - The message to display
+    """
+    while True:
+        print("\n%s\n" % message)
+        
+        userIn = input("Continue (y/n): ")
+        
+        if (userIn == "Y" or userIn == "y"):
+            return True
+        elif (userIn == "N" or userIn == "n"):
+            return False
+        else:
+            print("Invalid input!")
+
+
+def SearchLog(module="", msgType="", last=0):
+    """
+    Searches the log file with the given search options.
+    Only one option is used at once.
+    
+    Args:
+    module - The module to search for
+    msgType - The message type to search for
+    last - The number of most recent messages to display
+    """
+    if (not os.path.exists(Logger.LOG_FILE)):
+        print("\nThere is no log file!")
+    else:
+        with open(Logger.LOG_FILE) as file:
+            lines = file.readlines()
+        lines = [line.strip() for line in lines]
+        
+        print()
+        
+        numFound = 0
+        if (not module == ""):
+            for line in lines:
+                info = line.split(" - ")
+                
+                if (info[1] == module):
+                    print(line)
+                    numFound += 1
+        elif (not msgType == ""):
+            for line in lines:
+                info = line.split(" - ")
+                
+                if (info[2] == msgType):
+                    print(line)
+                    numFound += 1
+        elif (not last == 0):
+            lines.reverse()
+            printLines = []
+            
+            if (last > len(lines)):
+                last = len(lines)
+            
+            for i in range(0, last):
+                printLines.append(lines[i])
+                numFound += 1
+            
+            printLines.reverse()
+            
+            for line in printLines:
+                print(line)
+                    
+        if (numFound == 0):
+            print("No log messages found")
+        
+    print("\nPress ENTER to continue...")
+    input()
+
+
+def CleanMenu():
+    """
+    Displays the clean files menu.
+    """
+    cleanMenuOptions = [Metrics.METRICS_FILE_NAME, Metrics.PROG_DATA_FILE_NAME, Logger.LOG_FILE, "All", "Back"]
+    while True:
+        DisplayMenu("Which file(s) should be deleted?", cleanMenuOptions)
+        
+        userIn = input("\nEnter a selection: ")
+        
+        if (userIn == "1"):
+            if (Confirm("This action will permanently delete %s" % cleanMenuOptions[0])):
+                DeleteFile(cleanMenuOptions[0])
+            break
+        elif (userIn == "2"):
+            if (Confirm("This action will permanently delete %s" % cleanMenuOptions[1])):
+                DeleteFile(cleanMenuOptions[1])
+            break
+        elif (userIn == "3"):
+            if (Confirm("This action will permanently delete %s" % cleanMenuOptions[2])):
+                DeleteFile(cleanMenuOptions[2])
+            break
+        elif (userIn == "4"):
+            if (Confirm("This action will permanently delete all listed files")):
+                DeleteFile(cleanMenuOptions[0])
+                DeleteFile(cleanMenuOptions[1])
+                DeleteFile(cleanMenuOptions[2])
+            break
+        elif (userIn == "5"):
+            break
+        else:
+            DisplayInputError()
+            
+            
+def LogMenu():
+    """
+    Displays the search log menu.
+    """
+    logMenuOptions = ["Search by module", "Search by type", "View last", "Back"]
+    while True:
+        DisplayMenu("What would you like to do?", logMenuOptions)
+        
+        userIn = input("\nEnter a selection: ")
+        
+        if (userIn == "1"):
+            userIn = input("\nEnter a module name: ")
+            SearchLog(module=userIn)
+        elif (userIn == "2"):
+            userIn = input("\nEnter a type: ")
+            SearchLog(msgType=userIn)
+        elif (userIn == "3"):
+            userIn = input("\nHow many: ")
+            
+            if (userIn.isdigit() and int(userIn) >= 0):
+                SearchLog(last=int(userIn))
+            else:
+                "Invalid input! Expected a valid positive integer!"
+        elif (userIn == "4"):
+            break
+        else:
+            DisplayInputError()
+
+
+mainMenuOptions = ["Clean files", "Search log file", "Exit"]
+while True:
+    DisplayMenu("How can I help you today?", mainMenuOptions)
+    
+    userIn = input("\nEnter a selection: ")
+    
+    if (userIn == "1"):
+        CleanMenu()
+    elif (userIn == "2"):
+        LogMenu()
+    elif (userIn == "3"):
+        break
+    else:
+        DisplayInputError()
