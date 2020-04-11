a612c64993041c865b44d63a17d755c3dc059686
wFanatos <bicknell.william@gmail.com>
2020-04-11 13:47:20 -0400

    Style changes

--- Coachable_Laravel/laravel/resources/views/event.blade.php
+++ Coachable_Laravel/laravel/resources/views/event.blade.php
@@ -4,19 +4,20 @@
 
 <body onload="createCharts(JSON.parse('{{ json_encode($runData[0][1]) }}'), JSON.parse('{{ json_encode($runData[0][2]) }}'), JSON.parse('{{ json_encode($runData[0][3]) }}'));"/>
 <div class="container">
-  <ul class="nav flex-row nav-pills mb-3 justify-content-center" id="pills-tab" role="tablist">
-    @for($i = 0; $i < count($runData); $i++)
-      <li class="nav-item">
-        <a class="nav-link @if ($i == 0) active @endif" id="pills-run{{$i}}-tab" data-toggle="pill" href="#pills-run{{$i}}" role="tab" aria-controls="pills-run{{$i}}" aria-selected="@if ($i == 0) true @else false @endif" onclick="createCharts(JSON.parse('{{ json_encode($runData[$i][1]) }}'), JSON.parse('{{ json_encode($runData[$i][2]) }}'), JSON.parse('{{ json_encode($runData[$i][3]) }}'));">Run {{ $i + 1 }}</a>
-      </li>
-    @endfor
-  </ul>
+  
   <div class="row justify-content-center" style="width:100%">
       <div class="tab-content" id="v-pills-tabContent" style="width:80%">
         <div class="card-header text-center">
             <h1>{{$event->event_name}}</h1>
             <h2>{{$event->event_date}}</h2>
         </div>
+        <ul class="nav flex-row nav-pills mb-3 justify-content-center" id="pills-tab" role="tablist">
+          @for($i = 0; $i < count($runData); $i++)
+          <li class="nav-item">
+            <a class="nav-link @if ($i == 0) active @endif" id="pills-run{{$i}}-tab" data-toggle="pill" href="#pills-run{{$i}}" role="tab" aria-controls="pills-run{{$i}}" aria-selected="@if ($i == 0) true @else false @endif" onclick="createCharts(JSON.parse('{{ json_encode($runData[$i][1]) }}'), JSON.parse('{{ json_encode($runData[$i][2]) }}'), JSON.parse('{{ json_encode($runData[$i][3]) }}'));">Run {{ $i + 1 }}</a>
+          </li>
+          @endfor
+        </ul>
         <br/>
         @for($i = 0; $i < count($runData); $i++)
           <div class="tab-pane fade @if($i == 0) show active @endif" id="pills-run{{$i}}" role="tabpanel" aria-labelledby="pills-run{{$i}}-tab">
